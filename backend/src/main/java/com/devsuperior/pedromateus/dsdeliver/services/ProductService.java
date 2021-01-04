69428a0f0a7990e208cf7e85cdb76cce5c688e92
PedroMateus26 <pedromateusneto@gmail.com>
2021-01-04 15:32:17 -0300

    GET/products

---
+++ backend/src/main/java/com/devsuperior/pedromateus/dsdeliver/services/ProductService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.pedromateus.dsdeliver.services;
+
+import com.devsuperior.pedromateus.dsdeliver.ProductDTO;
+import com.devsuperior.pedromateus.dsdeliver.entities.Product;
+import com.devsuperior.pedromateus.dsdeliver.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<ProductDTO> findAll(){
+        List<Product> list = repository.findAllByOrderByNameAsc();
+        return list.stream().map(x->new ProductDTO(x)).collect(Collectors.toList());
+    }
+}
