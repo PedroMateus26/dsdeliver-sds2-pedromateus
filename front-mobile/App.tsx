1f928e41bcde9e825bcf3086f84c4bec7a4e5435
PedroMateus26 <pedromateusneto@gmail.com>
2021-01-11 09:54:15 -0300

    Add Home page

---
+++ front-mobile/App.tsx
@@ -0,0 +1,34 @@
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './Header';
+import Home from './Home';
+
+export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular, OpenSans_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+    return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Header/>
+      <Home/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+    backgroundColor: '#fff',
+   
+  },
+});
