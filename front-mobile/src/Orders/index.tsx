45f4a45325a1fb1bed3c896e806fc5e99496b248
PedroMateus26 <pedromateusneto@gmail.com>
2021-01-13 10:33:53 -0300

    Added order list

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import OrderCard from '../OrderCard'
 
 const Orders=()=>{
     return(
-        <View>
-            <Text>Listar Pedidos</Text>
+        <View style={styles.container}>
+            <OrderCard/>
         </View>
     )
 }
 
 export default Orders;
+
+const styles=StyleSheet.create({
+    container:{
+        paddingRight: '5%',
+        paddingLeft: '5%'
+    }
+})
